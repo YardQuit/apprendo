157ef534f5caaeff6c6125e72fed03964a4398df
Michael Jones <yardquit@pm.me>
2024-12-21 17:33:29 +0100

    Sat 21 Dec 2024 17:33:31 CET

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 RELEASE="$(rpm -E %fedora)"
 
 ### Set hostname
-echo A$(date +"%y%m") | tee /etc/hostname
+echo A$(date +"%y%m") > /tmp/system_files/etc/hostname
 
 ### Copy pre-configured system files
 rsync -rvK /tmp/system_files/ /
