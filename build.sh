0785fd53c5d8cd5e41797298a0d3421c44a2ff29
Michael Jones <yardquit@pm.me>
2024-12-22 10:41:58 +0100

    Sun 22 Dec 2024 22:42:00 NZDT

--- build.sh
+++ build.sh
@@ -10,10 +10,6 @@ rsync -rvK /tmp/system_files/ /
 ### Create system directory structues
 mkdir -p /var/lib/alternatives
 
-### Enable fstrim for encrypted drives
-cp /etc/crypttab /etc/crypttab.bak
-sed -i 's/none$/none discard/g' /etc/crypttab
-
 ### Install repository package(s)
 dnf install \
 $(cat /tmp/packages/desktop) \
@@ -59,23 +55,6 @@ sed -i 's/DefaultZone=FedoraWorkstation/DefaultZone=drop/g' /etc/firewalld/firew
 ### Add yubico challange for sudo (DISABLED)
 # cp /etc/pam.d/sudo /etc/pam.d/sudo.bak
 # sed -i '/PAM-1.0/a\auth       required     pam_yubico.so mode=challenge-response' /etc/pam.d/sudo
-
-### (VERY PERSONAL) Add encrypted drives to crypttab
-# sudo tee -a /etc/crypttab <<EOF
-# # manually added
-# luks-bccaf371-16c7-45fd-9b1a-1dc540d37218 UUID=bccaf371-16c7-45fd-9b1a-1dc540d37218 none discard
-# luks-4d1fa180-514b-4b2f-b028-4fbd80714ba0 UUID=4d1fa180-514b-4b2f-b028-4fbd80714ba0 none discard
-# luks-f4ba7b65-7a97-4661-9d37-333d0fc12582 UUID=f4ba7b65-7a97-4661-9d37-333d0fc12582 none discard
-# EOF
-
-# (VERY PERSONAL) Update fstab with encrypted drives
-# sudo cp /etc/fstab /etc/fstab.bak
-# sudo sed -i '\/home /d' /etc/fstab
-# sudo tee -a /etc/fstab <<EOF
-# UUID=a1f3db46-7cbc-4fe7-8940-da1238319914 	/home             btrfs   subvol=home,compress=zstd:1,x-systemd.device-timeout=0 0 0
-# UUID=d459a275-73d7-48a0-bcb2-b6b3272635e6 	/mnt/fstab_virt   ext4    defaults,x-systemd.device-timeout=0 1 2
-# UUID=2a8f21d0-de05-48dc-bf5b-6487504c4c07 	/mnt/fstab_stash  ext4    defaults,x-systemd.device-timeout=0 1 2
-# EOF
 
 ###############################################################################
 # END - ONLY FOR MY PERSONAL USE
